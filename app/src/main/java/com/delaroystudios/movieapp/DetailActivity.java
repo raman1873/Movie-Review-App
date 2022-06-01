2aa4263442dd6772b850c02a44595bd92e901162
raman1873 <ramandeepsingh14200@gmail.com>
2022-06-02 01:55:27 +0530

    Movie Review App

---
+++ app/src/main/java/com/delaroystudios/movieapp/DetailActivity.java
@@ -0,0 +1,357 @@
+package com.project.movieapp;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.github.ivbaranov.mfb.MaterialFavoriteButton;
+import com.project.movieapp.adapter.ReviewAdapter;
+import com.project.movieapp.adapter.TrailerAdapter;
+import com.project.movieapp.api.Client;
+import com.project.movieapp.api.Service;
+import com.project.movieapp.data.FavoriteContract;
+import com.project.movieapp.data.FavoriteDbHelper;
+import com.project.movieapp.model.Movie;
+import com.project.movieapp.model.Review;
+import com.project.movieapp.model.ReviewResult;
+import com.project.movieapp.model.Trailer;
+import com.project.movieapp.model.TrailerResponse;
+import com.takusemba.multisnaprecyclerview.MultiSnapRecyclerView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static com.project.movieapp.data.FavoriteContract.FavoriteEntry.COLUMN_TITLE;
+
+/**
+ * Created by delaroy on 5/18/17.
+ */
+public class DetailActivity extends AppCompatActivity {
+    TextView nameOfMovie, plotSynopsis, userRating, releaseDate;
+    ImageView imageView;
+    private RecyclerView recyclerView;
+    private TrailerAdapter adapter;
+    private List<Trailer> trailerList;
+    private FavoriteDbHelper favoriteDbHelper;
+    private Movie favorite;
+    private final AppCompatActivity activity = DetailActivity.this;
+    private SQLiteDatabase mDb;
+
+    Movie movie;
+    String thumbnail, movieName, synopsis, rating, dateOfRelease;
+    int movie_id;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //TODO
+        FavoriteDbHelper dbHelper = new FavoriteDbHelper(this);
+        mDb = dbHelper.getWritableDatabase();
+
+
+        imageView = (ImageView) findViewById(R.id.thumbnail_image_header);
+       // nameOfMovie = (TextView) findViewById(R.id.title);
+        plotSynopsis = (TextView) findViewById(R.id.plotsynopsis);
+        userRating = (TextView) findViewById(R.id.userrating);
+        releaseDate = (TextView) findViewById(R.id.releasedate);
+
+        Intent intentThatStartedThisActivity = getIntent();
+        if (intentThatStartedThisActivity.hasExtra("movies")){
+
+            movie = getIntent().getParcelableExtra("movies");
+
+            thumbnail = movie.getPosterPath();
+            movieName = movie.getOriginalTitle();
+            synopsis = movie.getOverview();
+            rating = Double.toString(movie.getVoteAverage());
+            dateOfRelease = movie.getReleaseDate();
+            movie_id = movie.getId();
+
+            String poster = "https://image.tmdb.org/t/p/w500" + thumbnail;
+
+            Glide.with(this)
+                    .load(poster)
+                    .placeholder(R.drawable.load)
+                    .into(imageView);
+
+          //  nameOfMovie.setText(movieName);
+            plotSynopsis.setText(synopsis);
+            userRating.setText(rating);
+            releaseDate.setText(dateOfRelease);
+
+       //TODO
+            ((CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar)).setTitle(movieName);
+
+        }else{
+            Toast.makeText(this, "No API Data", Toast.LENGTH_SHORT).show();
+        }
+
+       MaterialFavoriteButton materialFavoriteButton = (MaterialFavoriteButton) findViewById(R.id.favorite_button);
+
+        if (Exists(movieName)){
+            materialFavoriteButton.setFavorite(true);
+            materialFavoriteButton.setOnFavoriteChangeListener(
+                    new MaterialFavoriteButton.OnFavoriteChangeListener() {
+                        @Override
+                        public void onFavoriteChanged(MaterialFavoriteButton buttonView, boolean favorite) {
+                            if (favorite == true) {
+                                saveFavorite();
+                                Snackbar.make(buttonView, "Added to Favorite",
+                                        Snackbar.LENGTH_SHORT).show();
+                            } else {
+                                favoriteDbHelper = new FavoriteDbHelper(DetailActivity.this);
+                                favoriteDbHelper.deleteFavorite(movie_id);
+                                Snackbar.make(buttonView, "Removed from Favorite",
+                                        Snackbar.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+
+        }else {
+            materialFavoriteButton.setOnFavoriteChangeListener(
+                    new MaterialFavoriteButton.OnFavoriteChangeListener() {
+                        @Override
+                        public void onFavoriteChanged(MaterialFavoriteButton buttonView, boolean favorite) {
+                            if (favorite == true) {
+                                saveFavorite();
+                                Snackbar.make(buttonView, "Added to Favorite",
+                                        Snackbar.LENGTH_SHORT).show();
+                            } else {
+                                int movie_id = getIntent().getExtras().getInt("id");
+                                favoriteDbHelper = new FavoriteDbHelper(DetailActivity.this);
+                                favoriteDbHelper.deleteFavorite(movie_id);
+                                Snackbar.make(buttonView, "Removed from Favorite",
+                                        Snackbar.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+
+        }
+
+        initViews();
+
+    }
+
+    public boolean Exists(String searchItem) {
+
+        String[] projection = {
+                FavoriteContract.FavoriteEntry._ID,
+                FavoriteContract.FavoriteEntry.COLUMN_MOVIEID,
+                FavoriteContract.FavoriteEntry.COLUMN_TITLE,
+                FavoriteContract.FavoriteEntry.COLUMN_USERRATING,
+                FavoriteContract.FavoriteEntry.COLUMN_POSTER_PATH,
+                FavoriteContract.FavoriteEntry.COLUMN_PLOT_SYNOPSIS
+
+        };
+        String selection = FavoriteContract.FavoriteEntry.COLUMN_TITLE + " =?";
+        String[] selectionArgs = { searchItem };
+        String limit = "1";
+
+        Cursor cursor = mDb.query(FavoriteContract.FavoriteEntry.TABLE_NAME, projection, selection, selectionArgs, null, null, null, limit);
+        boolean exists = (cursor.getCount() > 0);
+        cursor.close();
+        return exists;
+    }
+
+
+    private void initViews(){
+        trailerList = new ArrayList<>();
+        adapter = new TrailerAdapter(this, trailerList);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view1);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+
+        loadJSON();
+        loadReview();
+    }
+
+    private void loadJSON(){
+        try{
+            if (BuildConfig.THE_MOVIE_DB_API_TOKEN.isEmpty()){
+                Toast.makeText(getApplicationContext(), "Please get your API Key", Toast.LENGTH_SHORT).show();
+                return;
+            }else {
+                Client Client = new Client();
+                Service apiService = Client.getClient().create(Service.class);
+                Call<TrailerResponse> call = apiService.getMovieTrailer(movie_id, BuildConfig.THE_MOVIE_DB_API_TOKEN);
+                call.enqueue(new Callback<TrailerResponse>() {
+                    @Override
+                    public void onResponse(Call<TrailerResponse> call, Response<TrailerResponse> response) {
+                        if (response.isSuccessful()) {
+                            if (response.body() != null) {
+                                List<Trailer> trailer = response.body().getResults();
+                                MultiSnapRecyclerView recyclerView = (MultiSnapRecyclerView) findViewById(R.id.recycler_view1);
+                                LinearLayoutManager firstManager = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false);
+                                recyclerView.setLayoutManager(firstManager);
+                                recyclerView.setAdapter(new TrailerAdapter(getApplicationContext(), trailer));
+                                recyclerView.smoothScrollToPosition(0);
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<TrailerResponse> call, Throwable t) {
+                        Log.d("Error", t.getMessage());
+                        Toast.makeText(DetailActivity.this, "Error fetching trailer", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+            }
+
+        } catch(Exception e){
+            Log.d("Error", e.getMessage());
+            Toast.makeText(this,e.toString(),Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    //TODO
+    private void loadReview(){
+        try {
+            if (BuildConfig.THE_MOVIE_DB_API_TOKEN.isEmpty()) {
+                Toast.makeText(getApplicationContext(), "Please get your API Key", Toast.LENGTH_SHORT).show();
+                return;
+            } else {
+                Client Client = new Client();
+                Service apiService = Client.getClient().create(Service.class);
+                Call<Review> call = apiService.getReview(movie_id, BuildConfig.THE_MOVIE_DB_API_TOKEN);
+
+                call.enqueue(new Callback<Review>() {
+                    @Override
+                    public void onResponse(Call<Review> call, Response<Review> response) {
+                        if (response.isSuccessful()){
+                            if (response.body() != null){
+                                List<ReviewResult> reviewResults = response.body().getResults();
+                                MultiSnapRecyclerView recyclerView2 = (MultiSnapRecyclerView) findViewById(R.id.review_recyclerview);
+                                LinearLayoutManager firstManager = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false);
+                                recyclerView2.setLayoutManager(firstManager);
+                                recyclerView2.setAdapter(new ReviewAdapter(getApplicationContext(), reviewResults));
+                                recyclerView2.smoothScrollToPosition(0);
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<Review> call, Throwable t) {
+
+                    }
+                });
+            }
+
+        } catch (Exception e) {
+            Log.d("Error", e.getMessage());
+            Toast.makeText(this, "unable to fetch data",Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    public void saveFavorite(){
+        favoriteDbHelper = new FavoriteDbHelper(activity);
+        favorite = new Movie();
+
+        Double rate = movie.getVoteAverage();
+
+
+        favorite.setId(movie_id);
+        favorite.setOriginalTitle(movieName);
+        favorite.setPosterPath(thumbnail);
+        favorite.setVoteAverage(rate);
+        favorite.setOverview(synopsis);
+
+        favoriteDbHelper.addFavorite(favorite);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.detail_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.share:
+                shareContent();
+
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void shareContent(){
+
+        Bitmap bitmap =getBitmapFromView(imageView);
+        try {
+            File file = new File(this.getExternalCacheDir(),"logicchip.png");
+            FileOutputStream fOut = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, fOut);
+            fOut.flush();
+            fOut.close();
+            file.setReadable(true, false);
+            final Intent intent = new Intent(android.content.Intent.ACTION_SEND);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.putExtra(Intent.EXTRA_TEXT, movieName);
+            intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file));
+            intent.setType("image/png");
+            startActivity(Intent.createChooser(intent, "Share image via"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private Bitmap getBitmapFromView(View view) {
+        Bitmap returnedBitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(),Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(returnedBitmap);
+        Drawable bgDrawable =view.getBackground();
+        if (bgDrawable!=null) {
+            bgDrawable.draw(canvas);
+        }   else{
+            canvas.drawColor(Color.WHITE);
+        }
+        view.draw(canvas);
+        return returnedBitmap;
+    }
+
+
+}
